b4c99f23b38f5405057545e926df7f22e4f1f95d
omnicomsrl <cesare.ranzato@omnicom.bs.it>
2016-01-08 17:17:08 +0100

    aggiornato

---
+++ angularJSGetStarted/appGitHubViewer.module.js
@@ -0,0 +1,61 @@
+﻿/*
+// Racchiudi i componenti di Angular in una Immediately Invoked Function Expression (IIFE)
+//
+// evitare
+// logger.js
+angular
+    .module('app')
+    .factory('logger', logger);
+
+//La funzione logger è aggiunta come variabile globale
+function logger() { }
+
+//
+// consigliato
+//
+// non ci sono più variabili globali
+//
+logger.js
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('logger', logger);
+
+    function logger() { }
+})();
+*/
+
+
+(function () {
+
+    /*Definire 1 componente per file.
+    //evitare
+    angular
+        .module('app', ['ngRoute'])
+        .controller('SomeController', SomeController)
+        .factory('someFactory', someFactory);
+
+    function SomeController() { }
+
+    //consigliato
+
+    // app.module.js
+    angular
+          .module('app', ['ngRoute']);
+
+    //consigliato
+
+    // some.controller.js
+    angular
+          .module('app')
+          .controller('SomeController', SomeController);
+
+    function SomeController() { }
+    */
+
+    angular
+        .module('appGitHubViewer', []);
+
+})();
